e749f7e413a0541f8d0f7ca338f3934bc02f7d84
Weston <dogooddowell@protonmail.com>
2023-07-10 15:41:19 -0500

    Update App

--- components/GitHubLink.tsx
+++ components/GitHubLink.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function ThirdwebGuideFooter() {
-  const url = "https://github.com/thirdweb-example/edition-drop";
+  const url = "https://github.com/NFTEarth/earthpass-edition";
   return (
     <>
       <div
